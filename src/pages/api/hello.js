ad02b5ea426ca85d6333a3664fceededcc67906c
Ruofan Wong <meimprankster@gmail.com>
2024-04-16 12:19:47 +0800

    finish hello

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,1 +1,8 @@
-export default function handler(req, res) {}
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    req.body.name === "" && res.status(401).json({ error: "Missing name!" });
+    res.status(200).json({ msg: `Hello, ${req.body.name}` });
+  } else {
+    res.status(200).json({ msg: "Hello" });
+  }
+}
--- src/pages/hello.js
+++ src/pages/hello.js
@@ -1,5 +1,24 @@
 import { useState } from "react";
 
+async function sayHelloAsync(text) {
+  const response = await fetch("/api/hello", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name: text }),
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    return {
+      code: response.status,
+      error: await data.error,
+    };
+  }
+  return {
+    code: response.status,
+    msg: data.msg,
+  };
+}
+
 const HelloPage = () => {
   const [text, setText] = useState("");
   const [received, setReceived] = useState({
@@ -12,13 +31,13 @@ const HelloPage = () => {
     <main className="main-container">
       <h1>Hello Page</h1>
       {received.msg && (
-        <div className="bg-green-700 text-white p-2 m-2">
+        <div>
           <p>{received.code} Received:</p>
           <p>{received.msg}</p>
         </div>
       )}
       {received.error && (
-        <div className="bg-red-700 text-white p-2 m-2">
+        <div>
           <p>{received.code} Error:</p>
           <p>{received.error}</p>
         </div>
@@ -31,7 +50,14 @@ const HelloPage = () => {
             setText(event.target.value);
           }}
         />
-        <button>Say hello</button>
+        <button
+          onClick={async () => {
+            const data = await sayHelloAsync(text);
+            setReceived(data);
+          }}
+        >
+          Say hello
+        </button>
       </div>
     </main>
   );
